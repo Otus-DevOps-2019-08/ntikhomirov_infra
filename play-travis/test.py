1b01e11fc6bf46a741f62e7db970f17f3697f209
Nikolay Tikhomirov <TihomirovNV@gmail.com>
2019-09-27 21:16:10 +0300

    Исправил тест и добавил еще несколько для математических операций!

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,7 +3,10 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 - 1, 0)
+        self.assertEqual(10 + 10, 100)
+        self.assertEqual(50/0.5, 100)
 
 if __name__ == '__main__':
     unittest.main()
