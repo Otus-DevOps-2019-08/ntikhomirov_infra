259bd6ef7fb3ace7df661e0b13a0b4e24cdd0d4b
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-04 16:55:21 +0300

    Исправил юниты для системы

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,33 @@ EOF
 
 
 #Ужасный способа не придумал как добавить конфиг для nginx
-sudo echo 'server {' > /etc/nginx/conf.d/otus.confg
-sudo echo 'listen 10.164.0.2:80;' >> /etc/nginx/conf.d/otus.confg
-sudo echo 'server_name otus.nt33.ru' >> /etc/nginx/conf.d/otus.confg
-sudo echo 'location / {'  >> /etc/nginx/conf.d/otus.confg
-sudo echo '    proxy_pass http://127.0.0.1:9292/;' >> /etc/nginx/conf.d/otus.confg
-sudo echo '    proxy_set_header Host $host;' >> /etc/nginx/conf.d/otus.confg
-sudo echo '    proxy_set_header X-Real-IP  $remote_addr;' >> /etc/nginx/conf.d/otus.confg
-sudo echo '    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/conf.d/otus.confg
-sudo echo '    allow all;' >> /etc/nginx/conf.d/otus.confg
-sudo echo '  }' >> /etc/nginx/conf.d/otus.confg
-sudo echo '}' >> /etc/nginx/conf.d/otus.confg
+sudo bash -s <<EOF
+echo 'server {' > /etc/nginx/conf.d/otus.confg
+echo 'listen 10.164.0.2:80;' >> /etc/nginx/conf.d/otus.confg
+echo 'server_name otus.nt33.ru' >> /etc/nginx/conf.d/otus.confg
+echo 'location / {'  >> /etc/nginx/conf.d/otus.confg
+echo '    proxy_pass http://127.0.0.1:9292/;' >> /etc/nginx/conf.d/otus.confg
+echo '    proxy_set_header Host $host;' >> /etc/nginx/conf.d/otus.confg
+echo '    proxy_set_header X-Real-IP  $remote_addr;' >> /etc/nginx/conf.d/otus.confg
+echo '    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/conf.d/otus.confg
+echo '    allow all;' >> /etc/nginx/conf.d/otus.confg
+echo '  }' >> /etc/nginx/conf.d/otus.confg
+echo '}' >> /etc/nginx/conf.d/otus.confg
 
+echo '[Unit]' > /etc/systemd/system/otus.service
+echo 'Description=puma' >> /etc/systemd/system/otus.service
+
+echo '' >> /etc/systemd/system/otus.service
+echo '[Service]' >> /etc/systemd/system/otus.service
+echo 'ExecStart=/home/tihomirovnv/reddit/puma -p' >> /etc/systemd/system/otus.service
+echo 'KillMode=process' >> /etc/systemd/system/otus.service
+echo 'Restart=on-failure' >> /etc/systemd/system/otus.service
+echo 'Type=notify' >> /etc/systemd/system/otus.service
+echo '' >> /etc/systemd/system/otus.service
+echo '[Install]' >> /etc/systemd/system/otus.service
+echo 'WantedBy=multi-user.target' >> /etc/systemd/system/otus.service
+echo 'Alias=sshd.service' >> /etc/systemd/system/otus.service
+EOF
 
 #Деплой приложения
 git clone -b monolith https://github.com/express42/reddit.git
@@ -27,19 +42,6 @@ cd reddit && bundle install
 
 #А что puma не может иметь своего собственого юнита?
 
-sudo echo '[Unit]' > /etc/systemd/system/otus.service
-sudo echo 'Description=puma' >> /etc/systemd/system/otus.service
-
-sudo echo '' >> /etc/systemd/system/otus.service
-sudo echo '[Service]' >> /etc/systemd/system/otus.service
-sudo echo 'ExecStart=/home/tihomirovnv/reddit/puma -p' >> /etc/systemd/system/otus.service
-sudo echo 'KillMode=process' >> /etc/systemd/system/otus.service
-sudo echo 'Restart=on-failure' >> /etc/systemd/system/otus.service
-sudo echo 'Type=notify' >> /etc/systemd/system/otus.service
-sudo echo '' >> /etc/systemd/system/otus.service
-sudo echo '[Install]' >> /etc/systemd/system/otus.service
-sudo echo 'WantedBy=multi-user.target' >> /etc/systemd/system/otus.service
-sudo echo 'Alias=sshd.service' >> /etc/systemd/system/otus.service
 
 sudo systemctl daemon reload
 sydo systemctl start otus
