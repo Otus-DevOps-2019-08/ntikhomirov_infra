7f67859e093ca8f5a72f3256ef78fd3d64ab76c3
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-04 16:47:31 +0300

    Не могу понять причину, почему работает tzdata в интерактивном режиме

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 sudo apt update
+
+sudo bash -s <<EOF
+apt update
+export DEBIAN_FRONTEND=noninteractive
+ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+apt-get install tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+EOF
+
 #Инсталяция ruby и bundler
 sudo apt install -y ruby-full ruby-bundler build-essential
-#Проверяем версии 
+#Проверяем версии
 ruby -v
 bundler -v
