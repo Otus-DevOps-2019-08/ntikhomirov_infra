6ce59483a91a51d2005e82359297d85e2288fae2
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-07 09:36:48 +0300

    Подготовил все для PR

--- cloud-testapp/CreateVM-startup.sh
+++ cloud-testapp/CreateVM-startup.sh
@@ -19,5 +19,5 @@ gcloud beta compute --project=indigo-almanac-254221 \
   --boot-disk-type=pd-standard \
   --boot-disk-device-name=${VM_NAME} \
   --reservation-affinity=any \
-  --tags=http-server \
+  --tags=http-server,puma-server \
   --metadata-from-file startup-script="startup-script.sh"
