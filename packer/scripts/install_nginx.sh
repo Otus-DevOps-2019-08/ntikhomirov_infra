89671b248be2d76fdf8e209d3ab5c810a4ee3f8e
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-10 14:17:16 +0300

    packer script

--- packer/scripts/install_nginx.sh
+++ packer/scripts/install_nginx.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-apt update -y
-#Cтавим tzdata так как требует дополнительного вмешательства
-
-apt update
-export DEBIAN_FRONTEND=noninteractive
-ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-apt-get install tzdata
-dpkg-reconfigure --frontend noninteractive tzdata
 
 apt install nginx -y
 
