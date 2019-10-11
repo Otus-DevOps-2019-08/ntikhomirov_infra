97b4a484779a40010eb33757b8cc146c3ef475df
Nikolay Tikhomirov <TihomirovNV@gmail.com>
2019-10-11 09:21:04 +0300

    Синхронизация

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.8"
+  required_version = "0.12.10"
 }
 
 provider "google" {
@@ -11,4 +11,20 @@ provider "google" {
   project = "indigo-almanac-254221"
 
   region = "europe-west4-a"
+}
+
+resource "google_compute_instance" "app" {
+  name = "nginx"
+  machine_type = "f1-micro"
+  zone = "europe-west4-a"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-nginx-1570710004"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
