8a151df1c554d982d8b2a999ec3c379582a83346
Nikolay Tikhomirov <TihomirovNV@gmail.com>
2019-10-15 11:58:02 +0300

    server

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,24 +10,20 @@ provider "google" {
   region = "europe-west4-a"
 }
 
+
 resource "google_compute_instance" "app" {
   name = "nginx"
   machine_type = "f1-micro"
   zone = "europe-west4-a"
   boot_disk {
     initialize_params {
-<<<<<<< HEAD
-      image = "ubuntu-nginx-1571125476"
-=======
       image = "ubuntu-nginx-ntikhomirov"
->>>>>>> 522095bfc50da1b78017668aac38f0edb3fae732
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-        nat_ip = "otus"
+    access_config{
     }
   }
 }
