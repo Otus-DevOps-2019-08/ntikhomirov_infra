d002a80f2c5e61e1b76e562240cc68a456e56e73
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-15 10:43:17 +0300

    Обновление конфигурационных файлов packer

--- packer/scripts/install_nginx.sh
+++ packer/scripts/install_nginx.sh
@@ -12,5 +12,8 @@ mv /tmp/privkey.pem /etc/nginx/ssl
 
 mv /tmp/fullchain.pem /etc/nginx/ssl
 
+mv /tmp/monitor.py /opt/www/monitor/cgi-bin
+
+chown -R nginx:nginx /opt/www
 
 sudo systemctl enable nginx
