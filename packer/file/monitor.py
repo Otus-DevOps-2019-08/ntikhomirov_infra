2dc9473575c7a127bacc38bd7267600f364b34f6
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-15 14:31:38 +0300

    допилил скрипт мониторинга

--- packer/file/monitor.py
+++ packer/file/monitor.py
@@ -12,11 +12,14 @@ def ping(hostname):
       print( hostname,'is down!')
 
 def check_socket(host, port):
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        if sock.connect_ex((host, port)) == 0:
-            print (host, ":" , port, "Port is open")
-        else:
-            print (host, ":", port, "Port is not open")
+   response = os.system("ping -c 1 " + host)
+   if response == 0:
+           with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+                 if sock.connect_ex((host, port)) == 0:
+                         print (host, ":" , port, "Port is open")
+                 else:
+                         print (host, ":", port, "Port is not open")
+
 
 ping("nginx")
 check_socket("nginx",80)
