021eb3a79f939b7b587d0a42c2dfcf44b06a9746
Nikolay Tikhomirov <tihomirovnv@gmail.com>
2019-10-21 18:21:06 +0300

    test

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -29,7 +29,7 @@ resource "google_compute_instance" "puma" {
 
   provisioner "remote-exec" {
     inline = [
-      "sed -i 's/mongo-db/mongo-${var.stand}/g' /home/tihomirovnv/app/reddit/app.rb"
+      "sed -i 's/mongo-db/mongo-${var.stand}/g' /home/tihomirovnv/app/reddit/app.rb",
     ]
   }
 }
