30ae5d8fe4775520a775d1e98e5469e67404dae4
Nikolay Tikhomirov <ntikhomirov@ntikhomirov.bscmsc.ru>
2019-10-21 22:09:07 +0300

    готовим к сдачи ДЗ

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -6,6 +6,50 @@ terraform {
 provider "google" {
 
   version = "2.15"
+
   project = "${var.project_id}"
+
   region = "${var.region}"
+
+}
+
+module "mongodb" {
+  source = "./../modules/db"
+
+  image_name = "${var.image_name.mongo}"
+
+  region = "${var.region}"
+
+  public_key_path = "${var.public_key_path}"
+
+}
+
+module "puma" {
+  source = "./../modules/app"
+
+  image_name = "${var.image_name.puma}"
+
+  region = "${var.region}"
+
+  public_key_path = "${var.public_key_path}"
+
+  private_key_path = "${var.private_key_path}"
+
+  count_puma = "${var.count_puma}"
+
+}
+
+module "nginx" {
+  source = "./../modules/nginx"
+
+  image_name = "${var.image_name.nginx}"
+
+  region = "${var.region}"
+
+  private_key_path = "${var.private_key_path}"
+
+  public_key_path = "${var.public_key_path}"
+
+  count_puma = "${var.count_puma}"
+
 }
